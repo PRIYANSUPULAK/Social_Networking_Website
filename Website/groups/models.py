77b47a3ef8a155a0d2c49eb5e4a30e7fde1a20b9
PRIYANSUPULAK <priyansu.pulak30@gmail.com>
2019-10-02 23:17:05 +0530

    group's model created

--- Website/groups/models.py
+++ Website/groups/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+# slugify helps in removing any character which isn't alpha-numeric or underscores or hyphens
+# Mainly, slugify converts spaces into '-' for making it a url
+from django.utils.text import slugify
 
-# Create your models here.
+# pip install misaka
+# misaka helps in marking down (link links) inside the posts.
+import misaka
+
+# get_user_model funtion returns the user modeel thats currently active in this project
+# It basically helps in finding out which user is signed in or else no one is signed in.
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.Library()
+
+class Group(models.Model):
+    name = models.CharField(max_length = 255, unique = True)
+# Slugs are a “URL friendly” version of the product or category name. They are typically lowercase, contain no special characters and hyphens are used in place of spaces.
+# Unicode provides a unique code value for every character, regardless of the platform, program, or language.
+    slug = models.slugField(allow_unicode = True, unique = True)
+    description = models.TextField(blank = True, default = '')
+    description_html = models.TextField(editable = False, default = '', blank = True)
+    members = models.ManyToManyField(User, through = 'GroupMember')
+
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.discription)
+        # This super is same as of java. It is saving to the base class
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('group:single',kwargs = {'slug':self.slug})
+
+    class Meta:
+        ordering = ['name']
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, related_name = "memberships")
+    user = models.ForeignKey(User, related_name = "user_groups")
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta():
+        unique_together = ('group', 'user')
